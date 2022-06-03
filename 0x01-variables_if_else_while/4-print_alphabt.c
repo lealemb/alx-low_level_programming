f9aaeffabef00b00db811eed03765f51f4965909
lealemb <lealemb84@gmail.com>
2022-06-03 16:25:23 -0700

    4 tab

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,24 +11,17 @@
 
 
 int main(void)
-
 {
-
 char low, e, q;
 
 e = 'e';
-
 q = 'q';
-for (low = 'a'; low <= 'z'; low++)
 
+for (low = 'a'; low <= 'z'; low++)
 {
 if (low != e && low != q)
-
 putchar(low);	
 }
 putchar('\n');
 return (0);
-
 }
-
-
