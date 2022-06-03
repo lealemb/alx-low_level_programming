65f99e995aae4373a65199d55312eac0b7c361f6
lealemb <lealemb84@gmail.com>
2022-06-03 05:20:42 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>                                                                                                                 
+#include <unistd.h>                                                                                                                   
+/**                                                                      
+ * main -start point                                                                                                                   * Description:no print 
+ *
+ * Return: return 1 means succes                                                                                                
+ */                                                                                                                                   int main(void)                                                                                                                        
+{                                                                                                          
+        char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";                                   
+       
+       	write(2, str1, 59);                                                                                                           
+	return (1);
+}
