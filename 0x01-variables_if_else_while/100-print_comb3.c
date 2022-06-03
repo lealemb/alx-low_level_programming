223782ec946338de3f58020cbe26b817fd7692ac
lealemb <lealemb84@gmail.com>
2022-06-03 15:17:16 -0700

    comb

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,47 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-
-int main(void)
-
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(0)
 {
-
-int d, p;
-
-
-
-for (d = '0'; d < '9'; d++)
+int d;
+for (d = 0; d < 100; d++)
 {
-
-for (p = d + 1; p <= '9'; p++)
+putchar((d / 10) + '0');
+putchar((d % 10) + '0');
+if (d != 99)
 {
-
-if (p != d)
-putchar(d);
-																								
-putchar(p);
-
-															
-if (d == '8' && p == '9')
-																															continue;
-																															putchar(',');
-																															putchar(' ');																														
+putchar(',');
+putchar(' ');
 }
-
 }
-
-}
-
 putchar('\n');
-
-
-
 return (0);
-
 }
