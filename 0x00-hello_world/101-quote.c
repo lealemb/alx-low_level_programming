007e62fcb5120625775dc463df76229689baa137
lealemb <lealemb84@gmail.com>
2022-06-04 17:50:49 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,15 @@
-#include <stdio.h>                                                                                                                 
-#include <unistd.h>                                                                                                                   
-/**                                                                      
- * main -start point                                                                                                                   * Description:no print 
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main -start point                                                             * Description:no print 
  *
- * Return: return 1 means succes                                                                                                
- */                                                                                                                                   int main(void)                                                                                                                        
-{                                                                                                          
-char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";                                   
-       
+ * Return: return 1 means succes
+ */
+
+int main(void)
+{
+char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";                                        
 write(2, str1, 59);                                                                                                           
 return (1);
 }
