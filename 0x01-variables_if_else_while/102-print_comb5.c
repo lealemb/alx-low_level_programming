719cae17d3d3f55bec21b47a4dd75a5371ad9761
lealemb <lealemb84@gmail.com>
2022-06-04 17:28:24 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,11 +1,11 @@
-#include <stdio.h>                                                                                                          
-#include <stdlib.h>                                        
+#include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
-/**                                                                                                        
+/**
  * main - entry point
  *
- * Description                                                                                               
+ * Description: display character
  * Return: Always 0.
  */
 int main(void)
