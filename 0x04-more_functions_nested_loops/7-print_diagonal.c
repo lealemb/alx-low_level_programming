f6bba2ba25f31612467e57f6a865e2e9bb6f9d36
lealemb <lealemb84@gmail.com>
2022-06-11 02:14:04 +0300

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -16,6 +16,7 @@ void print_diagonal(int n)
 			for (space = 0; space < len; space++)
 				_putchar(' ');
 			_putchar('\\');
+			
 			if (len == n - 1)
 				continue;
 
@@ -25,4 +26,3 @@ void print_diagonal(int n)
 
 	_putchar('\n');
 }
-
