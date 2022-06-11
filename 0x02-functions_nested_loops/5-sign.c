d52461e44fd486836dc02ceab178fc9ce1e1ae4b
lealemb <lealemb84@gmail.com>
2022-06-11 12:58:38 +0300

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,16 +1,11 @@
 #include "main.h"                                                                             
 #include <stdio.h>                                                                                                                                              
-                                                                                                                                                                
-/**                                                                                                                                                             
-* more_numbers - Print numbers between 0 to 14 incl.                                                                                                            
-*                                                                                                                                                               
-* Return: Void.                                                                                                                                                 
-*/                                                                                                                                                              
-void more_numbers(void)                                                                                                                                         
-{                                                                                                                                                               
+/**                                                                                             * more_numbers - Print numbers between 0 to 14 incl.                                           *                                                                
+ * Return: Void.                                                                 
+ */                                                                                                                                                              
+void more_numbers(void)                                                                        {                                                                                                                                                               
 int i;                                                                                                                                                          
 int x;                                                                                                                                                          
-                                                                                                                                                                
 for (x = 0; x < 10; x++)                                                                                                                                        
 {                                                                                                                                                               
 i = 0;                                                                                                                                                          
@@ -21,8 +16,7 @@ if (i >= 10)
 _putchar(i / 10 + '0');                                                                                                                                         
 }                                                                                                                                                               
 _putchar(i % 10 + '0');                                                                                                                                         
-i++;                                                                                                                                                            
+i++;                                                                                                                                                           
 }
 _putchar('\n');                                                                                                                                                 
-}                                                                                                                                                               
-}
+}                                                                                              }
