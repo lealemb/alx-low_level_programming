77cb1684c680db19729f7f36d29222b31066e2c9
lealemb <lealemb84@gmail.com>
2022-06-21 02:27:48 +0300

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,18 +1,10 @@
-#include "main.h"                                                                                                                                
-/**                                                                                                                                              
- * _memset -starting of a function                                                                                                               
- * @s: pointer                                                                                                                                   
- * @n: intiger input                                                                                                                             
- * @b: character input                                                                                                                           
- *                                                                                                                                               
- * Return: the area of s                                                                                                                         
- */                                                                                                                                              
+#include "main.h"                                                  
+/**                                                                                            * _memset -starting of a function                                                  
+ * @s: pointer                                                                                 * @n: intiger input                                                  
+ * @b: character input                                                                         *   
+ * Return: the area of s                                                                       */                                                   
 char *_memset(char *s, char b, unsigned int n)                                                                                                   
-{                                                                                                                                                
-unsigned int i;                                                                                                                                  
-                                                                                                                                                 
+{                                                                                             unsigned int i;                                                   
 for (i = 0; i < n; i++)                                                                                                                          
-{                                                                                                                                                
-s[i] = b;                                                                                                                                        
-}                                                                                                                                                
-return (s);
+{                                                                                             s[i] = b;                                                   
+}                                                                                             return (s);
