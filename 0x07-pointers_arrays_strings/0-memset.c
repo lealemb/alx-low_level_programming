b8f5e5bea1d9292401fa0e62ad5d729ee82cfbe7
lealemb <lealemb84@gmail.com>
2022-06-21 02:35:29 +0300

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,8 +3,8 @@
  * @s: pointer                                                                                 * @n: intiger input                                                  
  * @b: character input                                                                         *   
  * Return: the area of s                                                                       */                                                   
-char *_memset(char *s, char b, unsigned int n)                                                                                                   
-{                                                                                             unsigned int i;                                                   
-for (i = 0; i < n; i++)                                                                                                                          
-{                                                                                             s[i] = b;                                                   
+char *_memset(char *s, char b, unsigned int n)                                                {                                                                                             unsigned int i;                                                   
+
+for (i = 0; i < n; i++)                                                                       {                                                                                             s[i] = b;                                                   
 }                                                                                             return (s);
+}
