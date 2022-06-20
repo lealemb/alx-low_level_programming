5ff4ef8be82e84ceb7f4b5b04aae9dd954bd5f05
lealemb <lealemb84@gmail.com>
2022-06-21 01:44:52 +0300

    0-memset.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef FILE_MAIN
-#define FILE_MAIN
+#ifndef FILE_main.h
+#define FILE_main.h
 
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
