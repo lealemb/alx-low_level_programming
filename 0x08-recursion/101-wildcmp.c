5da81a1e9aa1073a825e2362c4fd734f59cddc0f
lealemb <lealemb84@gmail.com>
2022-06-27 15:36:59 +0300

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -88,4 +88,13 @@ int wildcmp(char *s1, char *s2)
 	{
 		iterate_wild(&s2);
 		s2 = postfix_match(s1, s2);
+	}
 
+	if (*s2 == '\0')
+		return (1);
+
+	if (*s1 != *s2)
+		return (0);
+
+	return (wildcmp(++s1, ++s2));
+}
