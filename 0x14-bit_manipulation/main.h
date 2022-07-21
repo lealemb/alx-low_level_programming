1981d102e7817fa5bb96bdfd8dbf7314dcdc2d07
lealemb <lealemb84@gmail.com>
2022-07-21 22:33:26 +0300

    0-binary_to_uint.c

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -5,7 +5,7 @@
  * File: main.h
  * AUth: Lealem
  * Desc: Header file containing prototypes for all functions
- *       written in the 0x13-bit_manipulation directory.
+ *       written in the 0x14-bit_manipulation directory.
  */
 
 int _putchar(char c);
